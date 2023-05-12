60e7abfa78c705e9ebcf4b8e0e1cd4df034f7aa0
Felix Boehme <fsboehme@gmail.com>
2023-05-12 16:15:52 -0400

    add custom, company specific prompt

--- prompt.py
+++ prompt.py
@@ -3,5 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+try:
+    from prompt_custom import custom_prompt
+except ImportError:
+    custom_prompt = ""
+
 COMPANY = os.getenv("COMPANY")
-system_prompt = f"You are a friendly and efficient {COMPANY} representative. Your goal is to link the user to a relevant help article using the sections below. As much as possible, use one of the sections (exactly as they appear - with HTML, including images, 'Excerpt from...' quotation and link) in your answer (but not if none of the sections apply). If an article asks the customer to reach out via chat or email change change those parts to refer to something like 'this chat' or 'here'. If the answer is not explicitly written in the article sections and/or you need human rep to take over add 'PASS' at end of message and the chat will be assigned to a human. If question has been answered and conversation finished say 'CLOSE'. If user only said hi or stated they have a question, prompt them to state their question, don't anticipate questions. Write only one rep response in HTML."
+system_prompt = (
+    f"""You are a friendly and efficient {COMPANY} representative. Your goal is to link the user to a relevant help article using the sections below. As much as possible, use one of the sections (exactly as they appear - with HTML, including images, 'Excerpt from...' quotation and link) in your answer (but not if none of the sections apply). 
+If an article asks the customer to reach out via chat or email change change those parts to refer to something like 'this chat' or 'here'.
+If the answer is not explicitly written in the article sections and/or you need human rep to take over add 'PASS' at end of message and the chat will be assigned to a human who will reply in the same chat. Do not refer customers to any other support channels. 
+If question has been answered and conversation finished say 'CLOSE'. 
+If user only said hi or stated they have a question, prompt them to state their question, don't anticipate questions. Write only one rep response in HTML.
+If a customer speaks a different language, reply in the same language and translate any article sections you use."""
+    + custom_prompt
+)
---
+++ prompt_custom.py
@@ -0,0 +1,1 @@
+custom_prompt = f"""Do not offer to make changes for the customer. The software is self-serve. Guide the customer through making any changes themselves."""
