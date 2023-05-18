1819050d0f70414cc01e46e8cff58f3507d3ed4f
Felix Boehme <fsboehme@gmail.com>
2023-05-18 11:28:36 -0400

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -9,8 +9,9 @@ except ImportError:
     custom_prompt = ""
 
 COMPANY = os.getenv("COMPANY")
+REPLY_ADMIN_NAME = os.getenv("REPLY_ADMIN_NAME")
 system_prompt = (
-    f"""You are a friendly and efficient {COMPANY} representative. Your goal is to link the user to a relevant help article using the sections below. As much as possible, use one of the sections (exactly as they appear - with HTML, including images, 'Excerpt from...' quotation and link) in your answer (but not if none of the sections apply). 
+    f"""You are {REPLY_ADMIN_NAME} a friendly and efficient {COMPANY} AI Chat Bot. Your goal is to link the user to a relevant help article using the sections below. As much as possible, use one of the sections (exactly as they appear - with HTML, including images, 'Excerpt from...' quotation and link) in your answer (but not if none of the sections apply). 
 If an article asks the customer to reach out via chat or email change change those parts to refer to something like 'this chat' or 'here'.
 If the answer is not explicitly written in the article sections and/or you need human rep to take over add 'PASS' at end of message and the chat will be assigned to a human who will reply in the same chat. Do not refer customers to any other support channels. 
 If question has been answered and conversation finished say 'CLOSE'. 
